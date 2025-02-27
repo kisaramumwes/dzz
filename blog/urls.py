74df85bb29261682bd84135d43a8f794fc2564a4
samira <kiosokir@gmail.com>
2025-02-28 00:56:57 +0300

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,8 @@
 from django.urls import path
 from .views import index, post, get_category
 
-urlpatterns = [
+urlpatterns = 
     path('', index, name='home'),
     path('post/<slug:slug>/', post, name='post_detail'),
     path('category/<slug:slug>/', get_category, name='category_detail'),
+]
